d0d7a28b10a9915fac56f4dbfd357a21df18c8bb
gbertl <gilbertlcsndle@gmail.com>
2022-12-06 17:11:14 +0800

    update HeroPopover

--- src/components/HeroPopover.tsx
+++ src/components/HeroPopover.tsx
@@ -1,17 +1,19 @@
-import shorts from '../assets/images/shorts.png';
+import { Link } from 'react-router-dom';
+import productShort5 from '../assets/images/product-short-5.png';
 
 const HeroPopover = () => {
   return (
     <div className="hero__popover">
       <div className="hero__popover-img">
-        <img src={shorts} alt="" />
+        <img src={productShort5} alt="" />
       </div>
 
       <div className="hero__popover-body">
         <h1 className="hero__popover-title">Shorts CLR</h1>
         <ul className="hero__popover-desc">
           <li>
-            Collection: <span className="font-bold">KLASSIK OF COLORS</span>
+            Collection:{' '}
+            <span className="font-bold uppercase">Fable of colors</span>
           </li>
           <li>
             Article: <span className="font-bold">H0146027</span>
@@ -29,9 +31,9 @@ const HeroPopover = () => {
           <span>
             Price: <span className="font-bold">$9</span>
           </span>
-          <a href="#" className="font-bold">
+          <Link to="/products/5" className="font-bold">
             Show more
-          </a>
+          </Link>
         </div>
       </div>
     </div>
