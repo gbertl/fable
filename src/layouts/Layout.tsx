30c4577c895d14f285c25e7ac0e54eb64b44b38d
gbertl <gilbertlcsndle@gmail.com>
2022-12-08 15:16:14 +0800

    add ScrollRestoration

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,9 +1,11 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, ScrollRestoration } from 'react-router-dom';
 import Header from './Header';
 
 const Layout = () => {
   return (
     <>
+      <ScrollRestoration />
+
       <Header />
       <main>
         <Outlet />
