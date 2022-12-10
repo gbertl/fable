78bbd0df8858c96b0be622b73855bbd3e872d536
gbertl <gilbertlcsndle@gmail.com>
2022-12-10 10:08:08 +0800

    implement empty cart alert

--- src/components/SideCart/index.tsx
+++ src/components/SideCart/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { MdChevronLeft } from 'react-icons/md';
 import { Link } from 'react-router-dom';
+import { BsCartX } from 'react-icons/bs';
+import { HashLink } from 'react-router-hash-link';
 
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { selectItems } from '../../store/slices/cart';
@@ -22,6 +24,8 @@ const SideCart = () => {
     }
   };
 
+  const length = cartItems.length;
+
   return (
     <div className="sidecart" onClick={handleClickOutside}>
       <div className="sidecart__body">
@@ -33,24 +37,44 @@ const SideCart = () => {
             <MdChevronLeft />
           </button>
           <h5 className="sidecart__heading">
-            Your Cart ({cartItems.length} items)
+            Your Cart ({length} item{length > 1 ? 's' : ''})
           </h5>
         </div>
-        <Cart className="sidecart__cart" />
 
-        <div className="sidecart__footer">
-          <p className="sidecart__footer-total">
-            Total: <span>₱{total}</span>
-          </p>
+        {length ? (
+          <>
+            <Cart className="sidecart__cart" />
 
-          <Link
-            to="/checkout"
-            className="btn btn-primary sidecart__checkout-btn"
-            onClick={() => dispatch(hideSideCart())}
-          >
-            Proceed to checkout
-          </Link>
-        </div>
+            <div className="sidecart__footer">
+              <p className="sidecart__footer-total">
+                Total: <span>₱{total}</span>
+              </p>
+
+              <Link
+                to="/checkout"
+                className="btn btn-primary sidecart__checkout-btn"
+                onClick={() => dispatch(hideSideCart())}
+              >
+                Proceed to checkout
+              </Link>
+            </div>
+          </>
+        ) : (
+          <div className="sidecart__alert">
+            <BsCartX className="sidecart__alert-icon" />
+            <h5 className="sidecart__alert-title">Your cart is empty</h5>
+            <p className="sidecart__alert-text">
+              You have no items in your shopping cart. Let's go buy something!
+            </p>
+            <HashLink
+              to="/#fable-of-colors-section"
+              className="btn btn-primary"
+              onClick={() => dispatch(hideSideCart())}
+            >
+              Shop Now
+            </HashLink>
+          </div>
+        )}
       </div>
     </div>
   );
