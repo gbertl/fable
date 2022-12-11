009411bd13be18f0d7aaef1c81b4ff8a86a89194
gbertl <gilbertlcsndle@gmail.com>
2022-12-11 20:06:20 +0800

    use tailwind in layouts

---
+++ src/layouts/Header.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
+
+import { Container } from '../components';
+import { useAppDispatch, useAppSelector } from '../hooks';
+import { selectItems } from '../store/slices/cart';
+import { showSideCart } from '../store/slices/ui';
+import logo from '../assets/icons/logo.svg';
+
+const mainNavLinks = [
+  {
+    url: '/#fable-of-colors-section',
+    text: 'Collections',
+  },
+  {
+    url: '#',
+    text: 'Customizer',
+  },
+  {
+    url: '#',
+    text: 'Sale',
+  },
+];
+
+const HeaderNav = ({ children }: { children: React.ReactNode }) => (
+  <ul className="uppercase font-medium flex gap-9">{children}</ul>
+);
+
+const Header = () => {
+  const cartItems = useAppSelector(selectItems);
+  const dispatch = useAppDispatch();
+
+  return (
+    <header className="text-sm">
+      <Container className="flex justify-between py-5 md:py-7">
+        <Link to="/" className="hidden md:block">
+          <img src={logo} alt="" />
+        </Link>
+        <HeaderNav>
+          {mainNavLinks.map((link, idx) => (
+            <li key={idx}>
+              <HashLink to={link.url}>{link.text}</HashLink>
+            </li>
+          ))}
+        </HeaderNav>
+
+        <HeaderNav>
+          <li>
+            <button
+              onClick={() => dispatch(showSideCart())}
+              style={{ textTransform: 'inherit' }}
+            >
+              Items ({cartItems.length})
+            </button>
+          </li>
+          <li>
+            <HashLink to="#">Profile</HashLink>
+          </li>
+        </HeaderNav>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
