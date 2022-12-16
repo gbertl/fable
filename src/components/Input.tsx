5ba6f767e1f3450d12861e5543e8f960b47d7726
gbertl <gilbertlcsndle@gmail.com>
2022-12-16 22:38:18 +0800

    handle refs in Input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,21 +4,27 @@ type Props<C extends React.ElementType> = {
   as?: C;
 } & React.ComponentPropsWithoutRef<C>;
 
-const Input = <C extends React.ElementType = 'input'>({
-  className = '',
-  as,
-  ...props
-}: Props<C>) => {
-  const Component = as || 'input';
+type PolymorphicRef<C extends React.ElementType> =
+  React.ComponentPropsWithRef<C>['ref'];
 
-  return (
-    <Component
-      className={`form-input ${className} ${
-        as === 'textarea' ? 'form-textarea' : ''
-      }`}
-      {...props}
-    />
-  );
-};
+const Input = React.forwardRef(
+  <C extends React.ElementType = 'input'>(
+    { className = '', as, ...props }: Props<C>,
+
+    ref?: PolymorphicRef<C>
+  ) => {
+    const Component = as || 'input';
+
+    return (
+      <Component
+        className={`form-input ${className} ${
+          as === 'textarea' ? 'form-textarea' : ''
+        }`}
+        {...props}
+        ref={ref}
+      />
+    );
+  }
+);
 
 export default Input;
