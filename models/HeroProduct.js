e0ff473aea5d445b9b07359a76618f74298107e4
gbertl <gilbertlcsndle@gmail.com>
2022-12-25 21:44:37 +0800

    add new column priorityOrder HeroProduct

--- models/HeroProduct.js
+++ models/HeroProduct.js
@@ -6,6 +6,7 @@ const schema = mongoose.Schema({
     ref: 'Product',
   },
   image: { type: String, required: true },
+  priorityOrder: { type: Number, default: 0 },
 });
 
 module.exports = mongoose.model('HeroProduct', schema);
