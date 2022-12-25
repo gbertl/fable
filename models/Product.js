dfdeb35e7681757dfd9deae1ca63d5f54eab8703
gbertl <gilbertlcsndle@gmail.com>
2022-12-25 12:03:44 +0800

    enable timestamps  for product model

--- models/Product.js
+++ models/Product.js
@@ -1,22 +1,25 @@
 const mongoose = require('mongoose');
 
-const schema = mongoose.Schema({
-  heroProduct: { type: mongoose.SchemaTypes.ObjectId, ref: 'HeroProduct' },
-  image: { type: String, required: true },
-  name: { type: String, required: true },
-  collectionName: { type: String, required: true },
-  category: {
-    type: mongoose.SchemaTypes.ObjectId,
-    ref: 'Category',
-    required: true,
+const schema = mongoose.Schema(
+  {
+    heroProduct: { type: mongoose.SchemaTypes.ObjectId, ref: 'HeroProduct' },
+    image: { type: String, required: true },
+    name: { type: String, required: true },
+    collectionName: { type: String, required: true },
+    category: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'Category',
+      required: true,
+    },
+    size: {
+      type: String,
+      enum: ['xs', 's', 'm', 'l', 'xl'],
+      required: true,
+    },
+    color: { type: String, required: true },
+    price: { type: Number, required: true },
   },
-  size: {
-    type: String,
-    enum: ['xs', 's', 'm', 'l', 'xl'],
-    required: true,
-  },
-  color: { type: String, required: true },
-  price: { type: Number, required: true },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Product', schema);
