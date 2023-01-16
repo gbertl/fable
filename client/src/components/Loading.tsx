83de8e8bb8f39a5b83428f9102678a8e5f12b71b
gbertl <gilbertlcsndle@gmail.com>
2023-01-16 15:19:29 +0800

    update loading text

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -12,7 +12,7 @@ const Loading = ({ isLoading }: { isLoading: boolean }) => {
           className="fixed top-0 left-0 w-full h-screen bg-white z-[9999] bg-opacity-90"
         >
           <h1 className="text-center text-2xl font-normal mt-[20%]">
-            Wait a moment while we load your app.
+            Wait a moment while we're fetching data.
           </h1>
         </motion.div>
       )}
