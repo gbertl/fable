39d0e6a3e2a46ec80a0b0cb6f6d578af85ee62fb
gbertl <gilbertlcsndle@gmail.com>
2024-11-28 13:44:52 +0800

    use VITE_AUTH0_AUDIENCE for baseURL

--- client/src/axios.ts
+++ client/src/axios.ts
@@ -1,12 +1,7 @@
 import axios from 'axios';
 
-const baseURL =
-  process.env.NODE_ENV === 'production'
-    ? 'https://scarlet-codfish-robe.cyclic.app/'
-    : 'http://localhost:5000/';
-
 const instance = axios.create({
-  baseURL,
+  baseURL: `${import.meta.env.VITE_AUTH0_AUDIENCE}/`,
 });
 
 export default instance;
